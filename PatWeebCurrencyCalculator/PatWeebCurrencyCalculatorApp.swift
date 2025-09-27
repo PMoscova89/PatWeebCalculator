07488dd6325cfbf5766da75c1f308057134affb3
Patrick Moscova <pmoscova89@gmail.com>
2025-09-27 16:43:48 -0400

    Initial Commit

---
+++ PatWeebCurrencyCalculator/PatWeebCurrencyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatWeebCurrencyCalculatorApp.swift
+//  PatWeebCurrencyCalculator
+//
+//  Created by Patrick Moscova on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct PatWeebCurrencyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PatWeebCurrencyCalculatorTests/PatWeebCurrencyCalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  PatWeebCurrencyCalculatorTests.swift
+//  PatWeebCurrencyCalculatorTests
+//
+//  Created by Patrick Moscova on 9/27/25.
+//
+
+import XCTest
+@testable import PatWeebCurrencyCalculator
+
+final class PatWeebCurrencyCalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
